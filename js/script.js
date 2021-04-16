ae22fdf0fc4214c081347aab4457a3093e178fe4
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-16 16:05:27 +0200

    Bonus: implementato funzione di ordinamento canzoni per anno di uscita

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@ function init() {
     new Vue({
         el: '#target',
         data: {
-            songsList: null,
-            filter: 0
+            songsList: null, // Lista di oggetti canzone
+            filter: 0 // Filtro per genere
         },
         mounted: function() {
             axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(data => {
                 this.songsList = data.data.response;
-                console.log(this.songsList);
             })
             .catch(() => {
                 console.log('Error');
             });
         },
         methods: {
+            // Filtro le canzoni e torno la lista al v-for
             filteredSongs: function () {
                 let filteredSongsList = [];
                 const songs = this.songsList;
@@ -25,7 +25,39 @@ function init() {
                 else{
                     filteredSongsList = this.songsList;
                 }
+
+                if(filteredSongsList != undefined){
+                    // Se ho una lista definita chiamo la funzione yearSort
+                    filteredSongsList = this.yearSort(filteredSongsList);
+                }
+
+                // Torno la lista filtrata ed orfinata
                 return filteredSongsList;
+            },
+            // Riaordino la lista per anno crescente
+            yearSort: function(filtered){
+                for (let i = 0; i < filtered.length; i++) {
+                    for (let j = 0; j < filtered.length; j++) {
+                        let song = filtered[j];
+                        let nextSong;
+
+                        // Se non mi trovo all'ultimo elemento
+                        if(j < filtered.length - 1){
+                            nextSong = filtered[j + 1];
+                        }else{
+                            nextSong = song;
+                        }
+                        
+                        // Se l'anno attivo è maggiore del seguente
+                        if(song.year > nextSong.year && nextSong != song){
+                            // Effetto lo scambio
+                            filtered[j+1] = song;
+                            filtered[j] = nextSong;
+                        }
+                    }
+                }
+                // Torno l'array ordinato
+                return filtered;
             }
         }
     });
